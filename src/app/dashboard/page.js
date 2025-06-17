76311c3da39a3acbe5f6c8e25c7f271dc72a7119
ardaaskinn01 <ardaaskindm@gmail.com>
2025-06-17 21:42:59 +0300

    last updates

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -640,7 +640,7 @@ export default function Dashboard() {
                     {showFavorites && (
                       <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
                         <div className="bg-white p-6 rounded shadow-lg max-w-md w-full">
-                          <h2 className="text-xl font-bold mb-4 text-center">Favori İlanlarım</h2>
+                          <h2 className="text-xl text-black font-bold mb-4 text-center">Favori İlanlarım</h2>
                           <ul className="space-y-3 max-h-64 overflow-y-auto">
                             {favorites.map((ilan, index) => (
                               <li
